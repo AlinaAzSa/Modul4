89e3205f29d8a751b32de9d579ca32e6d4412a89
SAM <Az.sadykov@yandex.ru>
2023-06-21 21:31:36 +0500

    my first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+word = input('Введите слово ')
+if str(word) == str(word)[::-1]:
+    print (True)
+else:
+    print(False)
+
+word1 = 'лепсспел'
+if str(word1) == str(word1)[::-1]:
+    print (True)
+else:
+    print(False)
+
+word1 = 'helloworld'
+if str(word1) == str(word1)[::-1]:
+    print (True)
+else:
+    print(False)
